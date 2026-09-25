f62842a4c06e88cc03fdef8f52f67b604b03401b
gustavo.oliveira <crodagustavo@gmail.com>
2026-09-25 16:57:49 -0300

    feat: adicionando parte dos módulos do curso

---
+++ app/Models/Modulo.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Modulo extends Model {
+    protected $fillable =['titulo', 'descricao', 'ordem'];
+
+    public function curso() {
+        return $this->belongsTo(Curso::class);
+    }
+}
